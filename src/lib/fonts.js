cccb35e80519e6ed808bf9c70331a9b2e09109cf
mohamed-ahmed10 <eng.mohamedahmed59@gmail.com>
2025-08-20 15:24:22 +0300

    Stable

---
+++ src/lib/fonts.js
@@ -0,0 +1,20 @@
+// src/lib/fonts.ts
+import { Gasoek_One, Sora, Space_Grotesk } from "next/font/google";
+
+export const gasoek = Gasoek_One({
+    weight: "400",
+    subsets: ["latin"],
+    display: "swap",
+});
+
+export const sora = Sora({
+    subsets: ["latin"],
+    weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+    display: "swap",
+});
+
+export const space = Space_Grotesk({
+    subsets: ["latin"],
+    weight: ["300", "400", "500", "600", "700"],
+    display: "swap",
+});
