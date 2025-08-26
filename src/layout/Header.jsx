b23187e67702ed9133a43bd3f8884868e3d16eab
mohamed-ahmed10 <eng.mohamedahmed59@gmail.com>
2025-08-26 12:16:37 +0300

    Edits

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,20 +1,22 @@
-"use client"
-import { usePathname } from 'next/navigation'
-import { useEffect } from 'react'
-import Nav from './Nav'
+"use client";
+import { usePathname } from 'next/navigation';
+import { useEffect } from 'react';
+import Nav from './Nav';
 
 export default function Header() {
-    const pathname = usePathname()
+    const pathname = usePathname();
+    // const t = useTranslations('common');
 
     useEffect(() => {
-        console.log(pathname)
-    }, [pathname])
+        console.log(pathname);
+    }, [pathname]);
+
     return (
         <header className="bg-[linear-gradient(135deg,rgba(232,48,48,0.45)_0%,rgba(232,48,48,0)_100%),url('/videos/header-bg.gif')] bg-no-repeat bg-cover w-full p-4">
             <Nav />
             <div className='mt-16'>
-                {pathname === '/' && 'This is extra content'}
+                {pathname === '/' && 'This is test'}
             </div>
         </header>
-    )
+    );
 }
--- src/layout/Nav.jsx
+++ src/layout/Nav.jsx
@@ -5,41 +5,52 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 export default function Nav() {
-
     const navItems = [
-        { href: '/', label: "Home" },
-        { href: '/portfolio', label: "Portfolio" },
-        { href: '/our-services', label: "Our services" },
-        { href: '/about-us', label: "About us" },
-        { href: '/contact-us', label: "Contact us" },
-    ]
+        { href: "/", label: "Home" },
+        { href: "/portfolio", label: "Portfolio" },
+        { href: "/our-services", label: "Our services" },
+        { href: "/about-us", label: "About us" },
+        { href: "/contact-us", label: "Contact us" },
+    ];
 
     const pathName = usePathname();
+
     return (
-        <div className="w-5/6 m-auto flex gap-4 sticky top-[4.89%] bottom-[82.61%]
+        <div
+            className="w-5/6 m-auto flex gap-4 sticky top-[4.89%] bottom-[82.61%]
         bg-[rgba(252,252,252,0.25)] border border-[rgba(255,255,255,0.15)]
         shadow-[0px_10px_24px_rgba(255,255,255,0.25)]
-        backdrop-blur-[17.5px] rounded-[70px] justify-between items-center px-2 py-4">
-            <Image src='/logo-2.webp' width={120} height={30} alt="Site logo" />
-            <ul className="flex gap-3">
-                {
-                    navItems.map((item, i) => {
-                        const isActive = item.href === pathName
-                        return (
-                            <li key={i}>
-                                <Link href={item.href} className={`px-3 py-2 rounded-full text-white font-semibold transition ${isActive
-                                    && "bg-[var(--main)] text-white"
-                                    }`}>{item.label}</Link>
-                            </li>
-                        )
-                    }
-                    )
-                }
+        backdrop-blur-[17.5px] rounded-[70px] justify-between items-center px-2 py-4"
+        >
+            <Image src="/logo-2.webp" width={120} height={30} alt="Site logo" />
+            <ul className="flex gap-3 relative">
+                {navItems.map((item, i) => {
+                    const isActive = item.href === pathName;
+                    return (
+                        <li key={i} className="relative">
+                            <Link
+                                href={item.href}
+                                className={`px-3 py-2 rounded-full font-semibold transition-all duration-700 ease-in-out ${isActive ? "bg-[var(--main)] text-white shadow-md" : "text-white/70 hover:text-white"}
+                `}
+                            >
+                                {item.label}
+                            </Link>
+                        </li>
+                    );
+                })}
             </ul>
             <div className="flex items-center gap-4">
-                <Image src='/icons/language.svg' className="cursor-pointer" width={30} height={30} alt="Language" />
-                <Button variant='link' href="/df">Git in touch</Button>
+                <Image
+                    src="/icons/language.svg"
+                    className="cursor-pointer"
+                    width={30}
+                    height={30}
+                    alt="Language"
+                />
+                <Button variant="link" href="/df">
+                    Git in touch
+                </Button>
             </div>
         </div>
-    )
+    );
 }
