1dcc7760f93baab5bb137e940da5dad7441dcce6
mohamed-ahmed10 <eng.mohamedahmed59@gmail.com>
2025-09-01 14:52:39 +0300

    To form not completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import ContactForm from "@/components/ContactForm";
 import { Button } from "@/components/ui/button";
 import { useTranslations } from "next-intl";
 import Image from "next/image";
@@ -27,7 +28,7 @@ export default function PAge() {
 
   return (
     <>
-      <div className="text-[var(--black-color)] container mx-auto">
+      <div className="text-[var(--black-color)] container mx-auto lg:p-6">
         <div className="grid grid-cols-2 my-8">
           <Image src="/images/home/at_sparks.webp" alt="At sparks" width={600} height={600} className="w-full" />
           <div className="flex justify-center flex-col">
@@ -49,7 +50,7 @@ export default function PAge() {
         <Image src="/images/home/2d.webp" alt="2D" width={600} height={500} className="w-3/4" />
         <div className="flex justify-center flex-col col-span-2">
           <h3 className="text-[3rem] font-bold">
-            <span className="text-[var(--main)]">2{t('d_video')} </span>
+            <span className="text-[var(--main)]">{t('2d_video')} </span>
             {t('production')}
           </h3>
           <p className="mt-6 font-semibold text-[1.5rem] text-center">{t('2d_desc')}</p>
@@ -60,9 +61,9 @@ export default function PAge() {
       </div>
       {/* 3D Video production */}
       <div className="grid grid-cols-3 my-10 gap-8 ps-10 lg:ps-24">
-        <div className="flex justify-center flex-col col-span-2 container mx-auto">
+        <div className="flex justify-center flex-col col-span-2 container mx-auto lg:p-6">
           <h3 className="text-[3rem] font-bold">
-            <span className="text-[var(--main)]">3{t('d_video')} </span>
+            <span className="text-[var(--main)]">{t('3d_video')} </span>
             {t('production')}
           </h3>
           <p className="mt-6 font-semibold text-[1.5rem] text-center">{t('3d_desc')}</p>
@@ -118,6 +119,30 @@ export default function PAge() {
           </div>
         </div>
       </div>
+      <div className="text-2xl text-center font-semibold">
+        <h4>{t('frames_of_our')}
+          <span className="text-[var(--main)]">{t('work')}</span>
+        </h4>
+        <p className="w-3/4 mx-auto">{t('our_work_desc')}</p>
+        <Button href="/portfolio" variant="foldable" className="my-10">{t('view_full_portfolio')}</Button>
+        <h3 className="text-8xl font-bold w-3/5 mx-auto flex justify-center items-center flex-wrap">
+          <span className="text-[var(--main)]">{t("we're")}</span>
+          {t('crafting')}
+          <span className="inline-block h-14 w-14 mx-2 rounded-sm bg-[radial-gradient(50%_50%_at_50%_50%,#0D0D0D_0%,#3D0404_100%)] mt-4 relative group">
+            <Image src="/images/samples/1.webp" width={500} height={500} alt="Sample" className="absolute -top-1/2 -left-1/2 z-50 opacity-0 group-hover:opacity-100 transition-all duration-500 max-w-52" />
+          </span>
+          {t('visual_sparks')}
+          <span className="inline-block h-14 w-14 mx-2 rounded-sm bg-[radial-gradient(50%_50%_at_50%_50%,#0D0D0D_0%,#3D0404_100%)] mt-4 relative group">
+            <Image src="/images/samples/2.webp" width={500} height={500} alt="Sample" className="absolute -top-1/2 -left-1/2 z-50 opacity-0 group-hover:opacity-100 transition-all duration-500 max-w-52" />
+          </span>
+          {t('that_bring')}
+          <span className="inline-block h-14 w-14 mx-2 rounded-sm bg-[radial-gradient(50%_50%_at_50%_50%,#0D0D0D_0%,#3D0404_100%)] mt-4 relative group">
+            <Image src="/images/samples/3.webp" width={500} height={500} alt="Sample" className="absolute -top-1/2 -left-1/2 z-50 opacity-0 group-hover:opacity-100 transition-all duration-500 max-w-52" />
+          </span>
+          <span className="text-[var(--main)]">{t("stories")}</span>
+          {t('to_life')}
+        </h3>
+      </div>
       <div className="h-40 my-16 relative">
         <div className="flex items-center overflow-hidden whitespace-nowrap bg-[var(--main)] h-12 rotate-2 absolute top-0 start-0 w-full">
           <div className="inline-block animate-marquee">
@@ -128,12 +153,16 @@ export default function PAge() {
         </div>
         <div className="flex items-center overflow-hidden whitespace-nowrap bg-[#EEEAEA] h-12 -rotate-2 absolute top-0 start-0 w-full mt-10">
           <div className="inline-block animate-marquee-reverse">
-            {hashTagsRed.map((item, i) =>
+            {hashTagsGray.map((item, i) =>
               <span key={i} className="text-[var(--main)] font-bold text-2xl px-10">{item}</span>
             )}
           </div>
         </div>
       </div>
+      <h3 className="text-8xl font-bold w-4/5 mx-auto flex justify-center items-center flex-wrap my-10">{t('we_driven_by_creative')}
+        <span className="text-[var(--main)]">{t('partners')}</span>
+      </h3>
+      <ContactForm />
     </>
   );
 }
---
+++ src/components/ContactForm.tsx
@@ -0,0 +1,160 @@
+"use client"
+
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { SelectGroup, SelectLabel } from "@radix-ui/react-select";
+import { useTranslations } from "next-intl";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import Swal from 'sweetalert2';
+import { z } from "zod";
+import { Button } from "./ui/button";
+import { Form, FormControl, FormField, FormItem, FormMessage } from "./ui/form";
+import { Input } from "./ui/input";
+const ContactForm = () => {
+    const t = useTranslations()
+    const router = useRouter();
+
+    const formSchema = z.object({
+        fullName: z.string().min(2, {
+            message: t("form.full_name_min"),
+        }),
+        phoneNumber: z
+            .string()
+            .min(6, {
+                message: t("form.phone_min"),
+            })
+            .regex(/^\+(\d{1,3})[-\s]?(\d{6,12})$/, {
+                message: t("form.phone_format"),
+            }),
+        email: z.string().email({
+            message: t("form.invalid_email"),
+        }),
+        projectType: z.string().min(2, {
+            message: t("form.company_name_min"),
+        }),
+    });
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            fullName: "",
+            email: "",
+            phoneNumber: "",
+            projectType: "",
+        },
+    });
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        console.log(values);
+        const Toast = Swal.mixin({
+            toast: true,
+            position: "top-end",
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+                toast.onmouseenter = Swal.stopTimer;
+                toast.onmouseleave = Swal.resumeTimer;
+            }
+        });
+        Toast.fire({
+            icon: "success",
+            title: t('contact_message')
+        });
+    }
+
+
+    return (
+        <div className="bg-[var(--dark-red)]">
+            <div className="container mx-auto lg:p-6 !text-white">
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                        <div className="grid md:grid-cols-2 gap-8 p-3">
+                            <div className="flex flex-col gap-10 text-3xl font-bold">
+                                <FormField
+                                    control={form.control}
+                                    name="fullName"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormControl>
+                                                <Input placeholder={t('form.full_name')} {...field} />
+                                            </FormControl>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+                                <FormField
+                                    control={form.control}
+                                    name="email"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormControl>
+                                                <Input placeholder={t('form.email')} {...field} />
+                                            </FormControl>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+                                <FormField
+                                    control={form.control}
+                                    name="phoneNumber"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormControl>
+                                                <Input placeholder={t('form.phone_number')} {...field} />
+                                            </FormControl>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+                                <FormField
+                                    control={form.control}
+                                    name="projectType"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormControl>
+                                                <Select onValueChange={field.onChange} defaultValue={field.value}>
+                                                    <SelectTrigger aria-invalid={!!form.formState.errors.projectType} >
+                                                        <SelectValue placeholder={t('form.project_type')} />
+                                                    </SelectTrigger>
+                                                    <SelectContent>
+                                                        <SelectGroup>
+                                                            <SelectLabel>{t('form.project_type')}</SelectLabel>
+                                                            <SelectItem value="two_d_animation">{t("project_types.two_d_animation")}</SelectItem>
+                                                            <SelectItem value="three_d_animation">{t("project_types.three_d_animation")}</SelectItem>
+                                                            <SelectItem value="media_production">{t("project_types.media_production")}</SelectItem>
+                                                            <SelectItem value="creative_solutions">{t("project_types.creative_solutions")}</SelectItem>
+                                                            <SelectItem value="film_making_production">{t("project_types.film_making_production")}</SelectItem>
+
+                                                        </SelectGroup>
+                                                    </SelectContent>
+                                                </Select>
+                                            </FormControl>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+                                <Button
+                                    type="submit"
+                                    variant="link"
+                                    size="lg"
+                                >
+                                    {t('submit')}
+                                </Button>
+                            </div>
+                            <div>test</div>
+                        </div>
+                    </form>
+                </Form>
+            </div>
+        </div>
+    );
+}
+
+export default ContactForm;
